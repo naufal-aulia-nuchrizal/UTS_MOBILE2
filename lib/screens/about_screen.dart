0936643a51ae2d5ae75c4bede52564f8515c41c6
naufal aulia nuchrizal 223PA <nofal3881@gmail.com>
2025-11-20 16:06:44 +0700

    Update about_screen.dart

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -7,7 +7,7 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("About App")),
-      body: Center(child: Text("Aplikasi Chatbot AAA\nDibuat oleh Naufal")),
+      body: Center(child: Text("Aplikasi Chatbot AAA\nDibuat oleh Naufal aulia nuchrizal\n 23552011366 CID B")),
     );
   }
 }
